12ea71ad09d9cc8be0b72962598d1ef0b860cfc4
Nazmuz Pranto <npranto@gmail.com>
2025-01-25 15:03:14 -0500

    [v.1] - Sets up basic Express app server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 // The main Express app lives here...
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use('/api/v1/data', require('./routes/dataRoutes'));
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 // This file starts the server
+require('dotenv').config();
+const mongoose = require('./config/db');
+const app = require('./app');
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
