0b2b4ebc09f3cf2022353a52db6e28498bc74516
Nazmuz Pranto <npranto@gmail.com>
2025-01-27 11:18:33 -0500

    [v.1] - Setup API routes for data

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,1 +1,13 @@
 // Define all routes related to data here...
+
+const express = require('express');
+const router = express.Router();
+const dataController = require('../controllers/dataController');
+
+router.post('/', dataController.createData);
+router.get('/', dataController.getAllData);
+router.get('/:id', dataController.getData);
+router.patch('/:id', dataController.updateData);
+router.delete('/:id', dataController.deleteData);
+
+module.exports = router;
